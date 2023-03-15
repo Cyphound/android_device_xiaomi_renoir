3d34073d8044c738bb543dd7a0e9b52889b97a66
Cyphound <cyphound@gmail.com>
2023-03-15 00:14:50 -0300

    renoir: PixelOS bringup

--- aosp_renoir.mk
+++ aosp_renoir.mk
@@ -11,25 +11,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common BananaDroid stuff.
-$(call inherit-product, vendor/banana/config/common.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Device Banana Flags.
-BANANA_MAINTAINER := Cyphound
-TARGET_ENABLE_BLUR := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
+# PixelOS flags
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-BANANA_BUILD_TYPE := UNOFFICIAL
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Device specific stuff
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi 11 Lite 5G
-PRODUCT_NAME := banana_renoir
+PRODUCT_NAME := aosp_renoir
 PRODUCT_SYSTEM_NAME := renoir_global
 PRODUCT_SYSTEM_DEVICE := renoir
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="renoir-user 13 RKQ1.211001.001 V14.0.6.0.TKICNXM release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := Xiaomi/renoir/renoir:13/RKQ1.211001.001/V14.0.6.0.TKICNXM:user/release-keys
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
